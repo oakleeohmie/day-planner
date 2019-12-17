2ce8d5af66d6abcabf64e376f528490e4164175e
Oaklee Ohmie <oakleeohmie@outlook.com>
2019-12-16 21:41:50 -0700

    styled buttons and time labels

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-var today = getHours();
-var nineAm = document.getElementById("nine").innerHTML = '';
 
-
-function getTime() {
-    if (today == [9]) {
-        nineAm.style.backgroundColor = "red";
-    } else if (today < [9]) {
-        nineAm.style.backgroundColor = "grey";
-    }
-    getTime();
-};
