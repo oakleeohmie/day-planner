fd881f099694c55f62a7d88ccb92232b1a845bcf
Oaklee Ohmie <oakleeohmie@outlook.com>
2019-12-18 22:40:38 -0700

    altered textarea ids

--- script.js
+++ script.js
@@ -1,64 +1,57 @@
 $(document).ready(function () {
-    // code below this
     var description = $('.description');
-
-    // when the button gets clicked
     $('.saveBtn').on('click', function () {
-        // want it to get the data in the input and save it to a new key
         makeKeys()
 
     })
-
-    // function for creating keys with every click
     function makeKeys() {
-        var comment = $.trim($('#plan-9').val());
+        var comment = $.trim($('#event-9').val());
         if (comment != '') {
-            localStorage.setItem('plan-1', comment)
+            localStorage.setItem('event-1', comment)
         }
-        var comment1 = $.trim($('#plan-10').val());
+        var comment1 = $.trim($('#event-10').val());
         if (comment1 != '') {
-            localStorage.setItem('plan-2', comment1)
+            localStorage.setItem('event-2', comment1)
         }
-        var comment2 = $.trim($('#plan-11').val());
+        var comment2 = $.trim($('#event-11').val());
         if (comment2 != '') {
-            localStorage.setItem('plan-3', comment2)
+            localStorage.setItem('event-3', comment2)
         }
-        var comment3 = $.trim($('#plan-12').val());
+        var comment3 = $.trim($('#event-12').val());
         if (comment3 != '') {
-            localStorage.setItem('plan-4', comment3)
+            localStorage.setItem('event-4', comment3)
         }
-        var comment4 = $.trim($('#plan-13').val());
+        var comment4 = $.trim($('#event-13').val());
         if (comment4 != '') {
-            localStorage.setItem('plan-5', comment4)
+            localStorage.setItem('event-5', comment4)
         }
-        var comment5 = $.trim($('#plan-14').val());
+        var comment5 = $.trim($('#event-14').val());
         if (comment5 != '') {
-            localStorage.setItem('plan-6', comment5)
+            localStorage.setItem('event-6', comment5)
         }
-        var comment6 = $.trim($('#plan-15').val());
+        var comment6 = $.trim($('#event-15').val());
         if (comment6 != '') {
-            localStorage.setItem('plan-7', comment6)
+            localStorage.setItem('event-7', comment6)
         }
-        var comment7 = $.trim($('#plan-16').val());
+        var comment7 = $.trim($('#event-16').val());
         if (comment7 != '') {
-            localStorage.setItem('plan-8', comment7)
+            localStorage.setItem('event-8', comment7)
         }
-        var comment8 = $.trim($('#plan-17').val());
+        var comment8 = $.trim($('#event-17').val());
         if (comment8 != '') {
-            localStorage.setItem('plan-9', comment8)
+            localStorage.setItem('event-9', comment8)
         }
     }
-    // need a function for printing after reload page
     function print() {
-        $('#plan-9').val(localStorage.getItem('plan-1'))
-        $('#plan-10').val(localStorage.getItem('plan-2'))
-        $('#plan-11').val(localStorage.getItem('plan-3'))
-        $('#plan-12').val(localStorage.getItem('plan-4'))
-        $('#plan-13').val(localStorage.getItem('plan-5'))
-        $('#plan-14').val(localStorage.getItem('plan-6'))
-        $('#plan-15').val(localStorage.getItem('plan-7'))
-        $('#plan-16').val(localStorage.getItem('plan-8'))
-        $('#plan-17').val(localStorage.getItem('plan-9'))
+        $('#event-9').val(localStorage.getItem('event-1'))
+        $('#event-10').val(localStorage.getItem('event-2'))
+        $('#event11').val(localStorage.getItem('event-3'))
+        $('#event-12').val(localStorage.getItem('event-4'))
+        $('#event-13').val(localStorage.getItem('event-5'))
+        $('#event-14').val(localStorage.getItem('event-6'))
+        $('#event-15').val(localStorage.getItem('event-7'))
+        $('#event-16').val(localStorage.getItem('event-8'))
+        $('#event-17').val(localStorage.getItem('event-9'))
     }
     function clearKeys() {
         localStorage.clear();
